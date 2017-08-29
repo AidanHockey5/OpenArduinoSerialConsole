c1186b8884884ee699a67f0fadca517f22c5c8fb
Aidan Lawrence <aidan@aidanlawrence.com>
2017-08-28 19:46:39 -0700

    Added 'Enter to Send' feature.

--- ConnectToArduino/ConnectToArduino/Form1.Designer.cs
+++ ConnectToArduino/ConnectToArduino/Form1.Designer.cs
@@ -143,6 +143,7 @@
             // 
             // ConnectToArduinoForm
             // 
+            this.AcceptButton = this.Send;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(307, 261);
